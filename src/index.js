91531efda540f96801a349dc16d6ef49f73d5b11
nooha mohammed <noohamohammed00@gmail.com>
2018-10-05 12:20:04 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -9,8 +9,7 @@ class MovieCard extends React.Component {
           <div className="movie-card">
         <img  
           src={this.props.poster} 
-          alt={this.props.title} 
-        />
+          alt={this.props.title} />
         <h2>{this.props.title}</h2>
         <small>Genres: {this.props.genres.join(', ')}</small>
       </div>
